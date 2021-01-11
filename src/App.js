b4a08ceacdfad02581a5eeaecb2ec668a9388a73
nikhilsati <nikhil121sati.5@gmail.com>
2021-01-11 14:58:34 +0530

    added paper

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import { Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core';
-import { Paper, Grid } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
+import Paper from './components/Paper/Paper';
 const useStyles = makeStyles({
 	root: {
 		// backgroundColor: "black"
@@ -14,22 +14,13 @@ function App() {
 		<div className={classes.root}>
 			<header className="App-header">
 				<Paper
-					elevation={0}
 					style={{
 						width: 1000,
-						height: 500,
-						backgroundColor: 'rgba(255, 255, 255, .15)',
-						backdropFilter: 'blur(5px)',
-						borderColor: 'rgba(255, 255, 255, .15)'
+						height: 500
 					}}
 				>
 					<Grid container justify="center" spacing={3}>
-						<Grid item xl={6}>
-							<Button>Hello</Button>
-							<Button variant="contained" color="secondary">
-								Hello
-							</Button>
-						</Grid>
+						<Grid item xl={6} />
 					</Grid>
 				</Paper>
 			</header>
