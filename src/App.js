e23ff4a1537c015f4aa90df5bfebd3b2a6209c70
Deepa Dangwal <dangwaldeepa19@gmail.com>
2021-01-13 15:26:15 +0530

    created icons,card,timeline

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { makeStyles } from '@material-ui/core';
 import { Paper, Grid } from '@material-ui/core';
 import Button from './components/button/Button';
 import Textbox from './components/textbox/Textbox';
+import Card from './components/card/Card';
+import Timeline from './components/timeline/Timeline';
 const useStyles = makeStyles({
 	root: {
 		// backgroundColor: "black"
@@ -14,27 +16,43 @@ function App() {
 	const classes = useStyles();
 	return (
 		<div className={classes.root}>
-			<header className="App-header">
-				<Paper
-					elevation={0}
-					style={{
-						width: 1000,
-						height: 500,
-						backgroundColor: 'rgba(255, 255, 255, .15)',
-						backdropFilter: 'blur(5px)',
-						borderColor: 'rgba(255, 255, 255, .15)'
-					}}
-				>
-					<Grid container justify="center" spacing={3}>
-						<Grid item xl={6}>
-							<Button color="error" rounded>
+			<Paper
+				elevation={0}
+				style={{
+					width: '70%',
+					backgroundColor: 'rgba(255, 255, 255, .15)',
+					backdropFilter: 'blur(5px)',
+					borderColor: 'rgba(255, 255, 255, .15)',
+					margin: 'auto'
+				}}
+			>
+				<Grid container justify="center" spacing={3}>
+					<Grid item xl={10}>
+						{/* <Button color="error" rounded>
 								Hello
 							</Button>
-							<Textbox color="success" fullwidth label="hello" />
-						</Grid>
+							<Textbox color="success" fullwidth label="hello" /> */}
+						{/* <Card
+							avatar={{
+								src: 'https://cdn.pixabay.com/photo/2016/12/19/21/36/winters-1919143_1280.jpg'
+							}}
+							title="new photo uploaded"
+							postImage="https://cdn.pixabay.com/photo/2015/01/07/15/51/woman-591576_1280.jpg"
+							date={new Date().toDateString()}
+						/>
+						<br />
+						<Card
+							avatar={{
+								src: 'https://cdn.pixabay.com/photo/2018/11/06/14/01/pair-3798371_1280.jpg'
+							}}
+							title="new photo uploaded"
+							postImage="https://cdn.pixabay.com/photo/2014/09/13/04/59/couple-443600_1280.jpg"
+							date={new Date().toDateString()}
+						/> */}
+						<Timeline />
 					</Grid>
-				</Paper>
-			</header>
+				</Grid>
+			</Paper>
 		</div>
 	);
 }
---
+++ src/components/icons/Icons.js
@@ -0,0 +1,26 @@
+import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUpRounded';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDownRounded';
+import AccountBalanceIcon from '@material-ui/icons/AccountBalanceRounded';
+import AddIcon from '@material-ui/icons/AddRounded';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorderRounded';
+import BrightnessIcon from '@material-ui/icons/Brightness1Rounded';
+import CameraIcon from '@material-ui/icons/CameraRounded';
+import FilterCenterFocusIcon from '@material-ui/icons/FilterCenterFocusRounded';
+import HomeIcon from '@material-ui/icons/HomeRounded';
+import SendIcon from '@material-ui/icons/SendRounded';
+import TagFacesIcon from '@material-ui/icons/TagFacesRounded';
+import LikeIcon from '@material-ui/icons/SentimentVerySatisfied';
+export {
+	ArrowDropUpIcon,
+	ArrowDropDownIcon,
+	AccountBalanceIcon,
+	AddIcon,
+	BookmarkBorderIcon,
+	BrightnessIcon,
+	CameraIcon,
+	FilterCenterFocusIcon,
+	HomeIcon,
+	SendIcon,
+	TagFacesIcon,
+	LikeIcon
+};
