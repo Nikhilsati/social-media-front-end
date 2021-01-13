c632e4b0100922dc9e4463c4f1dfdcc036a67f33
nikhilsati <nikhil121sati.5@gmail.com>
2021-01-13 15:26:09 +0530

    created icons, header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import { makeStyles } from '@material-ui/core';
 import { Grid } from '@material-ui/core';
-import Paper from './components/Paper/Paper';
+import Paper from './components/paper/Paper';
+import Header from './components/header/Header';
 const useStyles = makeStyles({
 	root: {
 		// backgroundColor: "black"
@@ -12,6 +13,7 @@ function App() {
 	const classes = useStyles();
 	return (
 		<div className={classes.root}>
+			<Header />
 			<header className="App-header">
 				<Paper
 					style={{
